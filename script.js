e5d872701c5a688071a9e0827f1291d36646c7b2
TheXilot <belfkih_medamine@yahoo.com>
2021-08-31 14:53:19 +0100

    commit

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 let line = 0;
 const fprint = (text) => {
     let e = document.createElement("p")
-    e.appendChild(document.createTextNode(`Print ${++line} : ` + text))
+    if(Array.isArray(text)){
+        let message = "[";
+        text.forEach(item => message+= item+",")
+        message += "]"
+        e.appendChild(document.createTextNode(`Print ${++line} : ` + message))
+    }else{
+        e.appendChild(document.createTextNode(`Print ${++line} : ` + text))
+    }
     document.getElementById("print").appendChild(e)
 }
 
@@ -65,18 +72,48 @@ const fprint = (text) => {
 // fprint(yearsUntilRetirement(1963,"BELFKIH Adil"));
 
 // Coding challenge #1 
-const calcAverage = (s1,s2,s3) => (s1+s2+s3)/3;
+// const calcAverage = (s1,s2,s3) => (s1+s2+s3)/3;
 
-const avgKoalas = calcAverage(44,23,71);
-const avgDolphins = calcAverage(85,54,41);
+// const avgKoalas = calcAverage(44,23,71);
+// const avgDolphins = calcAverage(85,54,41);
 
-function checkWinner(avgKoalas,avgDolphins){
-    if(avgDolphins>avgKoalas){
-        fprint(`the Dolphins team win (${avgDolphins} vs. ${avgKoalas})`)
-    }else if(avgDolphins<avgKoalas){
-        fprint(`the Koalas team win (${avgKoalas} vs. ${avgDolphins})`)
-    }else{
-        fprint(`No one win this party`)
-    }
-}
-checkWinner(avgKoalas,avgDolphins);
+// function checkWinner(avgKoalas,avgDolphins){
+//     if(avgDolphins>avgKoalas){
+//         fprint(`the Dolphins team win (${avgDolphins} vs. ${avgKoalas})`)
+//     }else if(avgDolphins<avgKoalas){
+//         fprint(`the Koalas team win (${avgKoalas} vs. ${avgDolphins})`)
+//     }else{
+//         fprint(`No one win this party`)
+//     }
+// }
+// checkWinner(avgKoalas,avgDolphins);
+
+//coding challenge
+
+//Arrays 
+const table = ['amine','said','ahmed']
+const tab = new Array("amine","said","ahmed")
+console.log(table);
+console.log(tab);
+// fprint(table.length)
+//array operations
+    //methods 
+        //push --Add element
+        tab.push("fahd");
+        fprint(tab);
+
+        //pop --remove the last element
+        tab.pop();
+        fprint(tab);
+
+        //indexOf --index of an element in the array -if not exist return -1
+        fprint(tab.indexOf('said'));
+        fprint(tab.indexOf('null'));
+
+        //includes --verify that an element exist in array
+        fprint(tab.includes('amine'));
+        fprint(tab.includes('null'));
+        
+
+
+//Arrays
